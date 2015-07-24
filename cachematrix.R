9702faebb0bee85b015667d433a87502e6c75a57
Daniel Machado <dalabarda@gmail.com>
2015-07-24 19:28:47 +0200

    Assignment 2, coursera R programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##  The function bellow store a list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x # get stores the matrix created 
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The function bellow allows storing the processed data from the  previous function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...) ## This line calculates the inverse of a square matrix
+        x$setinverse(m) 
+        m ## the invece of the matrix is cached here
 }
-
-
-## testando ete arquivo
+## Return a matrix that is the inverse of 'mat'
